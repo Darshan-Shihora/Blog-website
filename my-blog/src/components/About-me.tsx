0bd625c308e6205f0650041fcf79eeceff2a2158
darshan <darshan.shihora@prominentpixel.com>
2024-03-14 12:30:45 +0530

    Footer component

--- my-blog/src/components/About-me.tsx
+++ my-blog/src/components/About-me.tsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import img from "../../src/assests/profile.png";
 function AboutMe() {
-  return <div>About-me</div>;
+  return (
+    <div className="h-[70vh] text-center mt-8 mx-60 ">
+      <h1 className="h-auto text-3xl font-bold text-start py-8">About me</h1>
+      <div className="flex space-x-16">
+        <div className="flex-col text-start font-serif">
+          <p className="pb-4">
+            I'm a paragraph. Click here to add your own text and edit me. It’s
+            easy. Just click “Edit Text” or double click me to add your own
+            content and make changes to the font. Feel free to drag and drop me
+            anywhere you like on your page. I’m a great place for you to tell a
+            story and let your users know a little more about you.
+          </p>
+          <p className="pb-4">
+            This is a great space to write long text about your company and your
+            services. You can use this space to go into a little more detail
+            about your company. Talk about your team and what services you
+            provide.
+          </p>
+          <p className="pb-4">
+            Tell your visitors the story of how you came up with the idea for
+            your business and what makes you different from your competitors.
+            Make your company stand out and show your visitors who you are.
+          </p>
+          <p>Contact me:</p>
+          <a href="#" className="text-blue-600">
+            Info@mySite.com
+          </a>
+        </div>
+        <img className="h-50 w-64" src={img} alt="Image" />
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
