82b1bb733a8290b24d6321366afd3a1def0940c7
darshan <darshan.shihora@prominentpixel.com>
2024-03-16 11:51:00 +0530

    Storing form data into firebase and rendering it

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -1,11 +1,15 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  ActionFunction,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import Root from "./root/Root";
 import Home from "./components/Home";
 import AboutMe from "./components/About-me";
 import BlogList from "./components/BlogList";
 import BlogDetail from "./components/BlogDetail";
 import AddBlog from "./components/AddBlog";
-// import { action as addBlogAction } from "./components/BlogForm";
+import { action as addBlogAction } from "./components/BlogForm";
 
 function App() {
   // const { pathname } = useLocation();
@@ -18,25 +22,18 @@ function App() {
     {
       path: "/",
       element: <Root />,
-      //   children: [
-      //     { path: "", element: <Home /> },
-      //     {
-      //       path: "blog",
-      //       element: <BlogList />,
-      //     },
-      //     { path: "blog/:blogId", element: <BlogDetail /> },
-      //     { path: "about-me", element: <AboutMe /> },
-      //   ],
-      // },
       children: [
         { path: "", element: <Home /> },
         {
           path: "blog",
-          element: <Outlet />,
           children: [
             { path: "", element: <BlogList /> },
             { path: ":blogId", element: <BlogDetail /> },
-            { path: "new", element: <AddBlog /> },
+            {
+              path: "new",
+              element: <AddBlog />,
+              action: addBlogAction as ActionFunction,
+            },
           ],
         },
         { path: "about-me", element: <AboutMe /> },
